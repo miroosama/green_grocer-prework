2e46e599ea812d9973ed9ba499e5ede035d136ce
osama miro <o.miro650@gmail.com>
2017-12-28 20:16:44 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -11,40 +11,15 @@ def consolidate_cart(cart)
   end
 end
 
+
 def apply_coupons(cart, coupons)
-  coupons.each do |coupon|
-    name = coupon[:item]
-    if cart[name] && cart[name][:count] >= coupon[:num]
-      if cart["#{name} W/COUPON"]
-        cart["#{name} W/COUPON"][:count] += 1
-      else
-        cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
-        cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
-      end
-      cart[name][:count] -= coupon[:num]
-    end
-  end
-  cart
+  # code here
 end
 
 def apply_clearance(cart)
-  cart.each do |name, properties|
-    if properties[:clearance]
-      updated_price = properties[:price] * 0.80
-      properties[:price] = updated_price.round(2)
-    end
-  end
-  cart
+  # code here
 end
 
 def checkout(cart, coupons)
-  consolidated_cart = consolidate_cart(cart)
-  couponed_cart = apply_coupons(consolidated_cart, coupons)
-  final_cart = apply_clearance(couponed_cart)
-  total = 0
-  final_cart.each do |name, properties|
-    total += properties[:price] * properties[:count]
-  end
-  total = total * 0.9 if total > 100
-  total
+  # code here
 end
